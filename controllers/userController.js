fd14ce3c0e32a0765f5693787da1a7a3555a2c40
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-11-09 02:39:10 +0530

    Added the backend for user auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,79 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({messgae: "Register the user lmao"});
-})
+    console.log(req.body);
+    const {username, email, password} = req.body;
+    if(!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const userAvailable = await User.findOne({email});
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+    
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        username, 
+        email,
+        password: hashedPassword,
+    });
+
+    console.log("User has been created Successfully");
+
+    if(user) {
+        res.status(201).json({_id: user.id, email: user.email});
+    }
+    else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+    res.json({message: "Register the user lmao"});
+});
 
 //@desc Login a user
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({messgae: "Login the user"});
-})
+    const {email, password} = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user = await User.findOne({email});
+    
+    // compare password with hashed password
+    if ( user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            }            
+        }, process.env.ACCESS_TOKEN,
+        {expiresIn: "1m"}
+        );
+        res.status(200).json({accessToken}); 
+    }
+    else {
+        res.status(401);
+        throw new Error("Email or password not valid");
+    }
+});
 
 //@desc Current user
 //@route GET /api/users/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({messgae: "This is the current user"});
-})
+    res.json(req.user);
+});
 
 module.exports = {registerUser, loginUser, currentUser};
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
 
 // make the server
 const app = express();
 const port = process.env.PORT_NO || 5001;
+
+app.use(express.json()) 
+
+// connect the database
+connectDb();
 
 app.listen(port, () => {
     console.log(`Server is running on the port ${port}`); 
