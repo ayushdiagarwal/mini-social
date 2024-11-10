7f0322ce986a95b9302bee60356df9452f8d6b3b
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-11-10 22:25:12 +0530

    Fixed postview

--- frontend/src/pages/PostView.jsx
+++ frontend/src/pages/PostView.jsx
@@ -1,15 +1,34 @@
 // This will be the detailed post page
 // will include everything about the post -> title, body, likes, user, date, comment
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useFetcher, useParams } from 'react-router-dom'; 
+import axios from 'axios';
+
 
 export function PostView() {
-    console.log("hey");
+    const [post, setPost] = useState({}); 
     const { postId } = useParams(); // Gets `userId` from URL
+    
+    useEffect(() => {
+        // Fetch post data from the backend API
+        axios.get(`/api/posts/${postId}/`)
+            .then((response) => {
+                setPost(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching post:", error);
+            });
+    }, [postId]); 
+
     // get the post component from this postId
     return (
         <>
         <h3>hey this is the postview</h3>
         <p>This is the postId: {postId}</p>
+        <p>title: {post.title}</p>
+        <p>body: {post.body}</p>
+        <p>Created by: {post.user}</p>
+        <p></p>
         </>
     );
 };
